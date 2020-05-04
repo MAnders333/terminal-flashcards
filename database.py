5b36a05639c4675e96049d99fe0cb4d38086977a
MAnders333 <marc_anders@gmx.net>
2020-05-04 17:32:42 +0200

    bug fix

--- database.py
+++ database.py
@@ -11,31 +11,32 @@ class Database:
         self.db_name = "database.db"
         self.conn = sqlite3.connect(self.db_name)
         self.c = self.conn.cursor()
-        self.tables = self.c.execute(
-            "select name from sqlite_master where type='table'").fetchall()
+        self.tables = [x[0] for x in self.c.execute(
+            "select name from sqlite_master where type='table'").fetchall()]
 
     def create_table(self, table_name):
         table_name = table_name.lower()
         # create table with three columns: native = user's native language, target = language to be learned, difficulty = difficulty for user to recall target from memory
-        self.c.execute(
-            f"create table if not exists {table_name} (native text,target text,difficulty integer,unique(native,target))")
-        self.tables.append(table_name)
-
-    def get_data_to_import(self, file_name, table_name):
-        # extract data from csv file and check for duplicates in database
-        data_from_csv = pd.read_csv(file_name)
-        data_in_db = pd.DataFrame(self.c.execute(
-            f"select * from {table_name}").fetchall(), columns=['native', 'target', 'difficulty'])
-        data_to_import = pd.merge(data_from_csv, data_in_db, indicator=True, how="outer").query(
-            '_merge=="left_only"').drop('_merge', axis=1)
-        return data_to_import
+        if table_name not in self.tables:
+            self.c.execute(
+                f"create table if not exists {table_name} (native text,target text,difficulty integer,unique(native,target))")
+            self.tables.append(table_name)
 
     def import_data_from_csv(self, file_name, table_name):
         if file_name[-3:] == 'csv':
-            df = self.get_data_to_import(file_name, table_name)
+            # extract data from csv file and check for duplicates in database
+            data_from_csv = pd.read_csv(file_name)
+            data_in_db = pd.DataFrame(self.c.execute(
+                f"select * from {table_name}").fetchall(), columns=['native', 'target', 'difficulty'])
+            df = pd.merge(data_from_csv, data_in_db, indicator=True, how="outer").query(
+                '_merge=="left_only"').drop('_merge', axis=1)
             self.c.executemany(
                 f"insert into {table_name} values (?,?,?)", df.itertuples(index=False))
             self.conn.commit()
+
+    def get_data_from_table(self, table_name):
+        df = self.c.execute(f"select * from {table_name}").fetchall()
+        return df
 
 
 if __name__ == "__main__":
